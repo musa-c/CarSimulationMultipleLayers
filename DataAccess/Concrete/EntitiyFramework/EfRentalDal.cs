57f9cfe8c2ae455eb080b81760307dfe9a22250e
Musa Civelek <musacivelekx@gmail.com>
2023-07-23 04:18:15 +0300

    EfRentalDal added

--- DataAccess/Concrete/EntitiyFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntitiyFramework/EfRentalDal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntitiyFramework
 {
-    internal class EfRentalDal
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapContext> , IRentalDal
     {
     }
 }
