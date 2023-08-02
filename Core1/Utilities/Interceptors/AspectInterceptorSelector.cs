8a233444f8ef13859004d4448daad7c036360dba
Musa Civelek <musacivelekx@gmail.com>
2023-08-02 18:51:58 +0300

    some fixes

--- Core1/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core1/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -12,8 +12,12 @@ namespace Core.Utilities.Interceptors
     {
         public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
         {
+            //MethodInterceptionBaseAttribute'e sahip (class)type'nin attribute'lerini döndürür.
+            // true ise properties ve event'lerde dahil edilir. False ise göz ardı edilir.
             var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
                 (true).ToList();
+            //örn: Add metotunda sadece MethodInterceptionBaseAttribute özniteliğine sahip attribute'yi dödürür.
+            // true ise properties ve event'lerde dahil edilir. False ise göz ardı edilir.
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
--- Core1/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
+++ Core1/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -5,7 +5,7 @@ namespace Core.Utilities.Interceptors
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
     {
-        public int s { get; set; } // Priority(öncelik) hangi attribute önce çalışsın
+        public int Priority { get; set; } // Priority(öncelik) hangi attribute önce çalışsın
 
         public virtual void Intercept(IInvocation invocation)
         {
